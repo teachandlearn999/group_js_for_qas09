c6e485d22a5f1e4f68a34d4d9b373b4f3b467d7f
“Olga <obannikova@inbox.ru>
2024-10-09 17:05:27 +0300

    changes in kata 582746fa14b3892727000c4f

--- olgaBann.js
+++ olgaBann.js
@@ -7,3 +7,10 @@ function countDevelopers(list) {
     }
     return count;
     }
+
+console.log(countDevelopers([
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ]))
