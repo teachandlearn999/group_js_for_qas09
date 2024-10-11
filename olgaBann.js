cbfd363b03f84568c57933ae5b5e073ac5659594
Boris S <115181391+teachandlearn999@users.noreply.github.com>
2024-10-10 20:57:25 -0700

    Update olgaBann.js

--- olgaBann.js
+++ olgaBann.js
@@ -1,16 +1,17 @@
 function countDevelopers(list) {
     let count = 0;
     for (let i = 0; i < list.length; i++) {
-    if (list[i].continent === 'Europe' && list[i].language === 'JavaScript') {
-    count++;
-    }
+        if (list[i].continent === 'Europe' && list[i].language === 'HTML') {
+            count++;
+        }
     }
     return count;
-    }
+}
 
 console.log(countDevelopers([
     { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
     { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
     { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
-  ]))
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' },
+    { firstName: 'Smith', lastName: 'J.', country: 'USA', continent: 'NorthAmerica', age: 20, language: 'C#' }
+  ]));
