02fdca5e83597963a1ba73c957f3401004f3284b
Olga Tsoy <olgakim.us19@gmail.com>
2024-10-11 13:18:44 -0500

    Change initial file olgaT

--- olgaT.js
+++ olgaT.js
@@ -1,1 +1,8 @@
-console.log("Git practice");
+
+
+const numbers = [1, 2, 3];
+
+const result = numbers.map((element, index, array) => {
+    return `${element} + ${element}, Original Array: ${array}`;
+});
+console.log(result);
