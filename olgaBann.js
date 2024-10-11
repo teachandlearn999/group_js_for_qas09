6299427de492d56ba86ff78ef59b693d5fc8efac
Boris S <115181391+teachandlearn999@users.noreply.github.com>
2024-10-10 21:57:07 -0700

    adding sum function (#1)
    * adding sum function
    
    * add file
    
    * update file
    
    * add file
    
    * modifying countDevelopers function
    
    * modifying countDevelopers function
    
    ---------
    
    Co-authored-by: BorisS <javaforqes@gmail.com>
    Co-authored-by: StepanidaK <steshakirillina@yandex.ru>

--- olgaBann.js
+++ olgaBann.js
@@ -1,10 +1,11 @@
 function countDevelopers(list) {
     let count = 0;
     for (let i = 0; i < list.length; i++) {
-        if (list[i].continent === 'Europe' && list[i].language === 'HTML') {
+        if (list[i].continent === 'Europe' && list[i].language === 'JavaScript') {
             count++;
         }
     }
+
     return count;
 }
 
@@ -13,5 +14,6 @@ console.log(countDevelopers([
     { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
     { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
     { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' },
-    { firstName: 'Smithh', lastName: 'J.', country: 'USA', continent: 'NorthAmerica', age: 20, language: 'C#' }
-  ]));
+    { firstName: 'Smith', lastName: 'J.', country: 'USA', continent: 'NorthAmerica', age: 20, language: 'C#' },
+    { firstName: 'Doe', lastName: 'B.', country: 'Canada', continent: 'NAm', age: 25, language: 'Java' }
+]));
