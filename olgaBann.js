d44a71dfe1b6d00d68b86fbb2a563468d15e36ce
BorisS <javaforqes@gmail.com>
2024-10-10 21:23:59 -0700

    modifying countDevelopers function

--- olgaBann.js
+++ olgaBann.js
@@ -1,17 +1,66 @@
 function countDevelopers(list) {
-    let count = 0;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].continent === 'Europe' && list[i].language === 'HTML') {
-            count++;
-        }
-    }
-    return count;
+	let count = 0;
+	for (let i = 0; i < list.length; i++) {
+		if (
+			list[i].continent === "Europe" &&
+			list[i].language === "JavaScript"
+		) {
+			count++;
+		}
+	}
+
+	return count;
 }
 
-console.log(countDevelopers([
-    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
-    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' },
-    { firstName: 'Smith', lastName: 'J.', country: 'USA', continent: 'NorthAmerica', age: 20, language: 'C#' }
-  ]));
+console.log(
+	countDevelopers([
+		{
+			firstName: "Noah",
+			lastName: "M.",
+			country: "Switzerland",
+			continent: "Europe",
+			age: 19,
+			language: "JavaScript",
+		},
+		{
+			firstName: "Maia",
+			lastName: "S.",
+			country: "Tahiti",
+			continent: "Oceania",
+			age: 28,
+			language: "JavaScript",
+		},
+		{
+			firstName: "Shufen",
+			lastName: "L.",
+			country: "Taiwan",
+			continent: "Asia",
+			age: 35,
+			language: "HTML",
+		},
+		{
+			firstName: "Sumayah",
+			lastName: "M.",
+			country: "Tajikistan",
+			continent: "Asia",
+			age: 30,
+			language: "CSS",
+		},
+		{
+			firstName: "Smith",
+			lastName: "J.",
+			country: "USA",
+			continent: "NorthAmerica",
+			age: 20,
+			language: "C#",
+		},
+		{
+			firstName: "Doe",
+			lastName: "B.",
+			country: "Canada",
+			continent: "NAm",
+			age: 25,
+			language: "Java",
+		},
+	])
+);
